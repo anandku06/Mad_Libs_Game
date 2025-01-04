1df638d9620a5e7188c50d3767dc3ca1082ada6b
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-04 23:48:25 +0530

    Basic setup for the content

--- src/MadLibsGame.java
+++ src/MadLibsGame.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class MadLibsGame {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
+        String adj1, n1, adj2, v1, adj3;
     }
 }
